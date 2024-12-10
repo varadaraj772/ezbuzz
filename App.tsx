e46e86988e0d6b7a9a7d7e522bfe56fea0fc75a5
varadaraj772 <varadaraj277@gmail.com>
2024-12-10 11:13:01 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,95 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Platform,
+  Button,
+  NativeModules,
+  NativeEventEmitter,
 } from 'react-native';
+import EasebuzzCheckout from 'react-native-easebuzz-kit';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const callPaymentGateway = async () => {
+    const url = 'https://testpay.easebuzz.in/payment/initiateLink';
+    const body = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/json',
+      },
+      body: new URLSearchParams({
+        key: '',
+        txnid: '',
+        amount: '^[0-9.]*$',
+        productinfo: '',
+        firstname: '',
+        phone: '^(+d{1,4}[-]?)?d{5,20}$',
+        email: '',
+        surl: '',
+        furl: '',
+        hash: '',
+        udf1: '',
+        udf2: '',
+        udf3: '',
+        udf4: '',
+        udf5: '',
+        udf6: '',
+        udf7: '',
+        address1: '',
+        address2: '',
+        city: '',
+        state: '',
+        country: '',
+        zipcode: '',
+        show_payment_mode: '',
+        split_payments: '',
+        request_flow: '',
+        sub_merchant_id: '',
+        payment_category: '',
+        account_no: '',
+        ifsc: '',
+      }),
+    };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    try {
+      const response = await fetch(url, body);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+
+    // var options = {
+    //   access_key: '',
+    //   pay_mode: 'test',
+    // };
+    // EasebuzzCheckout.open(options)
+    //   .then(data => {
+    //     //handle the payment success & failed response here
+    //     console.log('Payment Response:');
+    //     console.log(data);
+    //   })
+    //   .catch(error => {
+    //     //handle sdk failure issue here
+    //     console.log('SDK Error:');
+    //     console.log(error);
+    //   });
+  };
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Button title="Call Payment Gateway" onPress={callPaymentGateway} />
+      <Text>App</Text>
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
